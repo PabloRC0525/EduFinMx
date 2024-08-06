bca6100e4051df496af0094c48998eb54ad68665
PabloRC0525 <121038827@upq.edu.mx>
2024-08-06 03:19:54 -0600

    users

--- database/migrations/2024_08_04_195645_create_users_table.php
+++ database/migrations/2024_08_04_195645_create_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
 {
-    Schema::create('Users', function (Blueprint $table) {
+    Schema::create('users', function (Blueprint $table) {
         $table->id();
         $table->string('Nombre');
         $table->string('ApellidoPaterno');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Users');
+        Schema::dropIfExists('users');
     }
 };
